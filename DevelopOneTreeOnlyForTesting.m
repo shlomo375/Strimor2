7f182da24c4b4d75680f7a2fe21c5c9f9906d437
shlomo375 <shlomo375@gmail.com>
2022-11-23 14:25:52 +0200

    fix bug of Inf minos Inf in the zone compere procces.

--- DevelopOneTreeOnlyForTesting.m
+++ DevelopOneTreeOnlyForTesting.m
@@ -11,7 +11,7 @@ Info.RowNumData.function = "";
 Info.RowNumData.parameter = 1;
 
 % ""C:\Users\ShlomoOdem\triangles\RRTtree\Results\23N\uniform_IM2Axis__3\tree_1""
-TreeFolder = fullfile(SoftwareLocation,"RRTtree","Results","18N","uniform_IM3AxisZone__3","tree_20");
+TreeFolder = fullfile(SoftwareLocation,"RRTtree","Results","16N","uniform_IM3AxisZoneInf__3","tree_1");
 dir(TreeFolder)
 tic
 Expend2Tree(TreeFolder,Info);
