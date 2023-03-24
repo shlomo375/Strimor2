be8c5396a5c7f6d521c600cb7001695a15ba308c
ShlomoOdem <shlomo.odem@gmail.com>
2023-03-24 17:46:51 +0300

    build basic selector
    the selector divide the action to 3 option:
    * remove module
    * switch edges
    * add module.

--- SimpleShapeAlg/GroupMatrixMatching.m
+++ SimpleShapeAlg/GroupMatrixMatching.m
@@ -5,10 +5,11 @@ function [Config_A, Config_B] = GroupMatrixMatching(Config_A,Config_B)
 GroupSizes_A = Config_A.IsomorphismMatrices1{1}(:,:,1);
 GroupSizes_B = Config_B.IsomorphismMatrices1{1}(:,:,1);
 if size(GroupSizes_A,1) > size(GroupSizes_B,1)
-    Config_B.IsomorphismMatrices1{1} = [Config_B.IsomorphismMatrices1{1}; zeros(size(GroupSizes_A,1)-size(GroupSizes_B,1),size(GroupSizes_B,2))];
-
+%     Config_B.IsomorphismMatrices1{1} = [Config_B.IsomorphismMatrices1{1}; zeros(size(GroupSizes_A,1)-size(GroupSizes_B,1),size(GroupSizes_B,2))];
+    Config_B.IsomorphismMatrices1{1} = [zeros(size(GroupSizes_A,1)-size(GroupSizes_B,1),size(GroupSizes_B,2)); Config_B.IsomorphismMatrices1{1}];
 else
-    Config_A.IsomorphismMatrices1{1} = [Config_A.IsomorphismMatrices1{1}; zeros(size(GroupSizes_B,1)-size(GroupSizes_A,1),size(GroupSizes_A,2))];
+%     Config_A.IsomorphismMatrices1{1} = [Config_A.IsomorphismMatrices1{1}; zeros(size(GroupSizes_B,1)-size(GroupSizes_A,1),size(GroupSizes_A,2))];
+Config_A.IsomorphismMatrices1{1} = [zeros(size(GroupSizes_B,1)-size(GroupSizes_A,1),size(GroupSizes_A,2)); Config_A.IsomorphismMatrices1{1}];
 end
 
 
